8800a1468d5e87bd7fdb8c677d239b5985e9ff59
Nick Bramhall <n.j.bramhall@gmail.com>
2019-04-25 19:03:45 +0100

    Updated config for https

--- publishconf.py
+++ publishconf.py
@@ -11,8 +11,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'http://tms.nickbramhall.com'
-RELATIVE_URLS = False
+SITEURL = 'https://tms.nickbramhall.com'
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
