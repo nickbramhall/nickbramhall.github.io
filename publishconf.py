edf0e03e460a11dd5909ae7f383ab6ee40bcf16e
Nick Bramhall <n.j.bramhall@gmail.com>
2019-04-26 17:48:00 +0100

    Update config to sort absolute URL issue

--- publishconf.py
+++ publishconf.py
@@ -12,7 +12,8 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://tms.nickbramhall.com'
-RELATIVE_URLS = True
+ABSOLUTE_URL = SITEURL
+RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
