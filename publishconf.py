27326f5545577e37408f54af85f2bb8ab5e2fb6a
Nick Bramhall <n.j.bramhall@gmail.com>
2022-06-01 11:35:45 +0000

    Updated domain

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://tms.nickbramhall.com'
+SITEURL = 'https://tms.invertedworld.co.uk'
 ABSOLUTE_URL = SITEURL
 RELATIVE_URLS = False
 
